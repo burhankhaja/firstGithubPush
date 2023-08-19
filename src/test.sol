5f906e144e65e09b15234dbb16fccc014e98b552
Burhan khaja <118989617+burhankhaja@users.noreply.github.com>
2023-08-19 06:38:51 +0530

    Update test.sol

--- src/test.sol
+++ src/test.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 contract whoAmI {
     uint256 internal constant MILLIONS = 300;
 
-    function getNetworth() public pure returns (uint256) {
+    function getNetworth() public view returns (uint256) {
         return MILLIONS;
     }
 }
